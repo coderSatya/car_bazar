07bb2ad67d93b1677c20e70c61da696f4e28215f
Satya Prakash <satya.prakash@webskitters.in>
2023-11-02 19:24:31 +0530

    intial commit

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,25 @@
+import { SearchManufacturerProps } from '@/types'
+import React from 'react';
+import Image from 'next/image';
+import { Combobox, Transition } from '@headlessui/react';
+
+const SearchManufacturer = ({manufacturer, setManufacturer}:SearchManufacturerProps) => {
+  return (
+    <div className='search-manufacturer'>
+      <Combobox>
+        <div className='relative w-full'>
+           <Combobox.Button className="absolute top-[14px]">
+            <Image src="/car-logo.svg" 
+            width={20}
+            height={200}
+            className="ml-4"
+            alt="Car logo"
+            />
+            </Combobox.Button> 
+        </div>
+      </Combobox>
+    </div>
+  )
+}
+
+export default SearchManufacturer
